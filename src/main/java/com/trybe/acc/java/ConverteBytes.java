5b7b0790421e5b377bd06b9f02332d6c1deb1688
Marcos Albuquerque <desenvolvedorwebfrontend@gmail.com>
2022-07-28 14:43:10 -0300

    Initial commit.

--- src/main/java/com/trybe/acc/java/ConverteBytes.java
+++ src/main/java/com/trybe/acc/java/ConverteBytes.java
@@ -3,20 +3,27 @@ package com.trybe.acc.java;
 import java.util.Locale;
 import java.util.Scanner;
 
+/** Informa o valor de bytes em bits. */
 public class ConverteBytes {
-	
-	public static void main(String[] args) {
-			// Ler a entrada de um número pelo console
-			Scanner scan = new Scanner(System.in).useLocale(Locale.US);
-			
-			System.out.println("Digite um número:");
-			byte valor = scan.nextByte();
-			
-			// Seu código aqui
-			
-			
-			// Imprime a saída no console. OBS: descomente a próxima linha
-			//System.out.println(valor + " bytes correspondem a " + qtdBits + " bits");
 
-		}
+  /** Ler a entrada de um número pelo console. */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+    StringBuilder stringBuilder = new StringBuilder();
+
+    System.out.println("Digite um número:");
+    byte valor = scan.nextByte();
+    
+    scan.close();
+    
+    int qtdBits = valor * 8;
+    
+    StringBuilder frase = stringBuilder.append(valor)
+                                       .append(" bytes correspondem a ")
+                                       .append(qtdBits)
+                                       .append(" bits");
+
+    System.out.println(frase);
+
+  }
 }
